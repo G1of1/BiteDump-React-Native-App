ba2e5fe37b35bb5ce65bd731598f7c47800ca932
G1of1 <ngsuna24@gmail.com>
2025-07-17 22:18:22 -0400

    Update: Included new features such as auth and more

--- app/(auth)/SignIn.tsx
+++ app/(auth)/SignIn.tsx
@@ -1,13 +1,57 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { router } from 'expo-router'
+import { View, Text, Button, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { Link, router } from 'expo-router'
+import CustomInput from '@/components/CustomInput'
+import CustomButton from '@/components/CustomButton'
+import { signIn } from '@/lib/appwrite'
 
 
 const SignIn = () => {
+  const [isSumbitting, setIsSumbitting] = useState<boolean>(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+  const submit = async () => {
+    const { email, password } = form;
+    if(!form.email || !form.password) return Alert.alert("Error", "Please enter valid email & password.")
+    setIsSumbitting(true);
+    try {
+      await signIn({email, password});
+      Alert.alert("Success", "User signed in succesfully!😎");
+      router.replace('/');
+    }
+    catch(error : any) {
+      Alert.alert("Error", error.message)
+    }
+    finally {
+      setIsSumbitting(false);
+    }
+
+  }
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button title={"Sign Up"} onPress={()=> router.push("/SignUp")}></Button>
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+
+
+      <CustomInput 
+        placeholder="Enter your email" 
+        value={form.email}
+        onChangeText={(text)=> setForm((prev) => ({...prev, email: text}))}
+        label="Email"
+        keyboardType='email-address'
+        />
+        <CustomInput 
+        placeholder="Enter your password" 
+        value={form.password}
+        onChangeText={(text)=> setForm((prev) => ({...prev, password: text}))}
+        label="Password"
+        secureTextEntry={true}
+        />
+        <CustomButton title={"Sign In"} isLoading={isSumbitting} onPress={submit}/>
+        <View className="flex justify-center mt-5 flex-row gap-2">
+          <Text className="base-regular text-gray-100">Don't have an account?</Text>
+          <Link href="/(auth)/SignUp" className="base-bold text-primary">Sign Up</Link>
+        </View>
     </View>
   )
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
 import { Redirect, Slot } from 'expo-router'
+import useAuthStore from '@/store/auth.store'
 
-export default function _layout() {
-    const isAuthenticated: boolean = false;
+export default function TabLayout() {
+    const { isAuthenticated } = useAuthStore();
     if(!isAuthenticated) return <Redirect href="/(auth)/SignIn" />
   return <Slot/>
 }
