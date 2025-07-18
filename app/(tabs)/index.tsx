94bf176a91d5586775f733ed9a2c7e6203b469da
G1of1 <ngsuna24@gmail.com>
2025-07-17 22:19:03 -0400

    Update: Included new features such as auth and more

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,11 @@ import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-
 import { SafeAreaView } from "react-native-safe-area-context";
 import cn from 'clsx'
 import CartButton from "@/components/CartButton";
+import useAuthStore from "@/store/auth.store";
  
 export default function Index() {
+  const { user } = useAuthStore();
+  console.log(user);
   return (
     <SafeAreaView className="flex-1 bg-white">
       
