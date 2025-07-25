726a308d67cf7ffd0e870adcffd15691743e4d56
G1of1 <ngsuna24@gmail.com>
2025-07-24 21:49:26 -0400

    Update: Implemented tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,6 @@ import useAuthStore from "@/store/auth.store";
  
 export default function Index() {
   const { user } = useAuthStore();
-  
   return (
     <SafeAreaView className="flex-1 bg-white">
       
